e0431fe44235d90fab3a468d8e77319d798e7384
eduardo <eduardo@vert.run>
2026-08-02 12:40:58 -0300

    Initial Commit

---
+++ AirLyra/AirLyraApp.swift
@@ -0,0 +1,32 @@
+//
+//  AirLyraApp.swift
+//  AirLyra
+//
+//  Created by Eduardo Fabio Ayaviri Zuna on 02/08/2026.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct AirLyraApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ AirLyraTests/AirLyraTests.swift
@@ -0,0 +1,18 @@
+//
+//  AirLyraTests.swift
+//  AirLyraTests
+//
+//  Created by Eduardo Fabio Ayaviri Zuna on 02/08/2026.
+//
+
+import Testing
+
+struct AirLyraTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
